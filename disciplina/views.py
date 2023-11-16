e9545a4f059ad98bd20be99d944bea1295f97448
gwpoliveira <gwpoliveira@gmail.com>
2023-11-16 00:04:26 -0300

    Viws e inicio de URLS

--- disciplina/views.py
+++ disciplina/views.py
@@ -1,4 +1,3 @@
-from typing import Any
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from .models import Disciplina
--- projectTCC/settings.py
+++ projectTCC/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'curso',
     'disciplina',
     'tcc',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
